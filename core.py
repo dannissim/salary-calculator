6624f4d47754e1b0894f2cd482ff4bf82c6718d5
Dan Nissim <nissim.dan@gmail.com>
2018-05-29 22:19:57 +0300

    updated tax rates

--- core.py
+++ core.py
@@ -1,21 +1,24 @@
-# app that calculates your monthly salary and your savings after taking off law ordered expenses: Pension savings,
+# app that calculates your monthly salary and your savings after taking off by-law expenses: Pension savings,
 # "Keren hishtalmut", Health insurance, income tax, social insurance.
 
-PENSION_RATE = 0.05; #5% off bruto
-HEALTH_INS = 0.05;
+PENSION_RATE = 0.07; # 7% off bruto, must be at least 6%
+HEALTH_INS = [0.031, 0.05];
 HISHTALMUT_RATE = 0.025; #2.5% off bruto
 EMPLOYER_HISHTALMUT = 0.075;
-EMPLOYER_PENSION = 0.05;
-SOCIAL_INS = 0.05;
+EMPLOYER_PENSION = 0.125;
+SOCIAL_INS = [0.04, 0.07];
+AVG_INCOME = 9907;
+SIXTYPERC_AVG_INCOME = int(0.6 * AVG_INCOME); # social and health insurance fee blocks depend on 60% avg national income
 INCOMETAX_THRESH = 4837;
 TAX_BRACKET = [INCOMETAX_THRESH, 6240, 8950, 14360, 19960, 41530, 53490];
 TAX_RATES =[1, 0.9, 0.86, 0.8, 0.69, 0.65, 0.53, 0.5];
 
-def incomeTaxDeduction(bruto):
+
+def income_tax_deduction(bruto):
     arr = TAX_BRACKET + [bruto];
     arr.sort();
     j = arr.index(bruto);
-    if j==0:
+    if j == 0:
         return bruto;
     sum = INCOMETAX_THRESH;
     for i in range(1, j):
@@ -23,10 +26,23 @@ def incomeTaxDeduction(bruto):
     sum += TAX_RATES[j]*(bruto - TAX_BRACKET[j-1]);
     return sum;
 
-brutoIncome = (float)(input("enter (bruto) salary "));
-brutoAfterExpenses = brutoIncome - brutoIncome*(PENSION_RATE + HEALTH_INS + SOCIAL_INS + HISHTALMUT_RATE);
-netIncome = incomeTaxDeduction(brutoAfterExpenses);
-savings = brutoIncome*(PENSION_RATE + HISHTALMUT_RATE + EMPLOYER_HISHTALMUT +EMPLOYER_PENSION);
+
+# calculates fee for health or social insurance, flag true for health ins., flag false for social ins.
+def calc_ins_fee(bruto, flag):
+    if bruto < SIXTYPERC_AVG_INCOME:
+        return HEALTH_INS[0]*bruto if flag else SOCIAL_INS[0]*bruto;
+    sum = HEALTH_INS[0] if flag else SOCIAL_INS[0];
+    sum *= SIXTYPERC_AVG_INCOME;
+    sum += HEALTH_INS[1]*(bruto-SIXTYPERC_AVG_INCOME) if flag else SOCIAL_INS[1]*(bruto-SIXTYPERC_AVG_INCOME);
+    return sum;
+
+
+brutoIncome = float(input("enter (bruto) salary "));
+healthInsFee = calc_ins_fee(brutoIncome, True);
+socialInsFee = calc_ins_fee(brutoIncome, False);
+brutoAfterExpenses = brutoIncome - brutoIncome*(PENSION_RATE + HISHTALMUT_RATE) - healthInsFee - socialInsFee;
+netIncome = income_tax_deduction(brutoAfterExpenses);
+savings = brutoIncome*(PENSION_RATE + HISHTALMUT_RATE + EMPLOYER_HISHTALMUT + EMPLOYER_PENSION);
 print("net income is: " + str(netIncome) + "\nsavings are " + str(savings));
 
 
