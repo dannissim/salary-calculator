c733f40925f9905eb5068a9235074d53a9432655
Dan <nissim.dan@gmail.com>
2022-07-12 00:13:31 +0300

    put in function

--- src/graph_creator.py
+++ src/graph_creator.py
@@ -3,15 +3,21 @@ import numpy as np
 
 import salary_calculator
 
-x = np.arange(0.0, 50000.0, 10000.0)
-y = [salary_calculator.savings(i) for i in x]
-z = [salary_calculator.net_income(i) for i in x]
-l = [int(y[i] + z[i]) for i in range(len(y))]
-plt.plot(x, y, label='Savings')
-plt.plot(x, z, label='Net Income')
-plt.plot(x, l, label='Total: Savings + Net')
-plt.legend(loc='upper left')
-plt.xlabel('Gross Salary')
-plt.ylabel('₪')
-plt.savefig("graph1.png")
-plt.show()
+
+def main():
+    x = np.arange(0.0, 50000.0, 10000.0)
+    y = [salary_calculator.savings(i) for i in x]
+    z = [salary_calculator.net_income(i) for i in x]
+    l = [int(y[i] + z[i]) for i in range(len(y))]
+    plt.plot(x, y, label='Savings')
+    plt.plot(x, z, label='Net Income')
+    plt.plot(x, l, label='Total: Savings + Net')
+    plt.legend(loc='upper left')
+    plt.xlabel('Gross Salary')
+    plt.ylabel('₪')
+    plt.savefig("graph1.png")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
